2e5249127c565c6e426759ff46bdf23c7fd448f2
Dovydas Stirpeika <dovy91@gmail.com>
2018-04-01 18:05:15 -0500

    matrix transpose

--- src/matrix.ts
+++ src/matrix.ts
@@ -6,9 +6,9 @@ export class Matrix {
     cols: number;
     vals: number[][];
 
-    constructor({rows, cols, fromArray}: MatrixArgs) {
+    constructor({rows, cols, fromArray}: MatrixArgs = {rows: 2, cols: 1}) {
         if (fromArray) {
-            this.vals = fromArray;            
+            this.vals = fromArray.map((row) => [...row]);
         } else {
             this.vals = Array(rows).fill(null).map(() => Array(cols).fill(0));
         }
@@ -30,13 +30,20 @@ export class Matrix {
 
     randomize = () => this.forEveryRow(() => Math.floor(Math.random() * 10));
 
-    transpose = () => this.vals =  
+    transpose = () => {
+        const arr: number[][] = Array(this.cols).fill(null).map(() => []);
+
+        this.vals.forEach((row) => row.forEach((n, i) => arr[i].push(n)));
+
+        return this.vals = arr;
+    }
 }
 
-function dotProduct(mtrX: Matrix, mtrY: Matrix) {
+export function dotProduct(mtrX: Matrix, mtrY: Matrix) {
     if (mtrX.rows !== mtrY.cols || mtrX.cols !== mtrY.rows) {
         throw "check number of rows and cols on each matrix";
     }
+    
     const matrix = new Matrix({rows: mtrX.rows, cols: mtrY.cols});
 
     const arr = matrix.vals.map((r, row) => r.map((c, col) =>
